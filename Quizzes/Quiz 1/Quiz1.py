652b8cf1e3708e826209fd9a6f75b4f082ec895b
Will Franken <FrankenW@jbu.edu>
2021-02-09 15:29:03 -0600

    Initial commit

---
+++ Quizzes/Quiz 1/Quiz1.py
@@ -0,0 +1,36 @@
+lst = ['This is the first string in the list', 'This is the last string in the list']
+x = input('enter start, first, or end: ')
+
+x2 = x.lower()
+if x2 == 'start':
+    print(lst[0])
+
+if x2 == 'first':
+    print(lst[0])
+
+if x2 == 'end':
+    print(lst[-1])
+
+
+
+
+
+
+
+
+
+
+
+#while True:
+
+   # if x2 == 'start' or 'first':
+   #     print(lst[0])
+    #    break
+
+  #  if x2 == 'end':
+    #    print(lst[-1])
+     #   break
+
+   # else:
+     #   print("you have made an invalid choice")
+     #   break
