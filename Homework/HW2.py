ec89ff6379444e7e27262ae5ea6e7e16e2b5bd65
Will Franken <FrankenW@jbu.edu>
2021-02-11 09:31:13 -0600

    Working on homework 2

---
+++ Homework/HW2.py
@@ -0,0 +1,84 @@
+from typing import Optional, List, Any
+
+
+def number_length(num: int) -> int:
+    num = len(num)
+    print('Number_length = ', num)
+
+    """
+    Create a function that takes a number and returns its length.
+    :param num:
+    :return:
+
+    examples:
+    number_length(10) -> 2
+    number_length(5000) -> 4
+    number_length(0) -> 1
+    """
+    pass
+
+
+def list_of_multiples(num: int, length: Optional[int]) -> List[int]:
+    x = 0
+    while x <= length:
+        length[x] = num * (x + 1)
+    print(length)
+
+    """
+    Create a function that takes two numbers as arguments (num, length)
+    and returns a list of multiples of num until the list length reaches length.
+    :param num:
+    :param length:
+    :return:
+
+    list_of_multiples(7, 5) -> [7, 14, 21, 28, 35]
+    list_of_multiples(12, 10) -> [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
+    ist_of_multiples(4, None) -> []
+    """
+    pass
+
+
+def normalize(input_str) -> str:
+    print(input_str.lower() + '!')
+
+    """
+    Create a function that takes a string. If the string is all uppercase characters,
+     convert it to lowercase and add an exclamation mark at the end.
+
+    :param foo:
+    :return:
+    normalize("CAPS LOCK DAY IS OVER") -> "Caps lock day is over!"
+    normalize("Today is not caps lock day.") -> "Today is not caps lock day."
+
+    """
+    pass
+
+
+def cat_dog(num: int) -> str:
+    if (num % 3 == 0) and (num % 5 == 0):
+        print('CatDog')
+    elif num % 3 == 0:
+        print('Cat')
+    elif num % 5 == 0:
+        print('Dog')
+    else:
+        print(num)
+
+    """
+    Create a function that takes a number as an argument and returns "Cat", "Dog" or "CatDog".
+
+    If the number is a multiple of 3 the output should be "Cat".
+    If the number given is a multiple of 5, the output should be "Dog".
+    If the number given is a multiple of both 3 and 5, the output should be "CatDog".
+    If the number is not a multiple of either 3 or 5, the number should be output
+    on its own as shown in the examples below.
+    The output should always be a string even if it is not a multiple of 3 or 5.
+
+    :param num:
+    :return:
+    cat_dog(4) -> "4"
+    cat_dog(3) -> "Cat"
+    cat_dog(5) -> "Dog"
+    cat_dog(15) -> "CatDog"
+    """
+    pass
