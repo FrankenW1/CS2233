79954437d41daa017c642a9e8e0d06bd40f5e8a1
Will Franken <FrankenW@jbu.edu>
2021-02-28 19:47:32 -0600

    Fixed quiz1?

--- Testing/test.py
+++ Testing/test.py
@@ -1,9 +1,4 @@
-import pytest
-from quiz import quiz1
 
-x = ['a', 'z']
-
-
-def test(lst):
-    quiz1.first(x)
-    assert quiz1.first(x) == 'a'
+my_list = [-3, -2, -1, 0, 1, 2, 3]
+new_list = [ number - 4 for number in my_list if number <= 2 ]
+print(new_list)
